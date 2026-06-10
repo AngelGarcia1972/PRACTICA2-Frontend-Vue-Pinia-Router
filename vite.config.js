8786260616bcd0c1b091c068f768a7e0760ce799
AngelGarcia1972 <angeldaga.17@gmail.com>
2026-06-10 17:13:45 -0600

    ✨ Feat: se completo la practica 14

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { visualizer } from "rollup-plugin-visualizer";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+	plugins: [
+		vue(),
+		vueDevTools(),
+		visualizer({
+			open: true,
+			filename: "stats.html",
+			gzipSize: true,
+			brotliSize: true,
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+});
